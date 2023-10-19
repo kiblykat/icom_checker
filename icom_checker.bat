17e096229ebc4347223260517aa2383c63941636
muhammad.izzat.2.fadzlon@continental-corporation.com <muhammad.izzat.2.fadzlon@continental-corporation.com>
2023-10-19 23:44:34 +0800

    able to read line from AC file, todo: read from GC

--- icom_checker.bat
+++ icom_checker.bat
@@ -1,2 +1,22 @@
 @echo off
-echo "I like scripting"
+setlocal enabledelayedexpansion
+@echo "choose ur env: //todo"
+
+set "file_AC=D:\Projects\WHUD\AC\core\pkg\icom\icom_export.sdh"
+set "file_GC=D:\Projects\WHUD\GC\adapt\gen\tofac\core\pkg\icom\icom_export.sdh
+set "search_word=INTERFACEID"
+
+set "line1="
+set "line2="
+
+REM Find the line containing the search word/phrase in AC
+for /f "tokens=*" %%a in ('type "%file_AC%" ^| find /n "%search_word%"') do (
+    set "line1=%%a"
+    set "line1=!line1:*]=!"
+    echo !line1!
+    goto :found1
+)
+@echo %%a
+:found1
+
+@echo line1
