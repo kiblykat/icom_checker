e103aa75bcd1122f6817539941eb4ef4af8766b7
muhammad.izzat.2.fadzlon@continental-corporation.com <muhammad.izzat.2.fadzlon@continental-corporation.com>
2023-10-24 21:57:58 +0800

    created checks for folders, run getprg/sym

--- icom_checker.bat
+++ icom_checker.bat
@@ -6,9 +6,21 @@ echo Script started at %date% %time% >> %logFile%
 
 setlocal enabledelayedexpansion
 
-REM need to further streamline, just prompt AC folder. the relative folder to toFGC and toFAC can be hardcoded
+REM need to further streamline, just prompt AC/GC folder. the relative folder to toFGC and toFAC can be hardcoded
+:retryFolderAC
 set /p buildFolderAC=Choose build folder for AC:
+IF NOT EXIST "%buildFolderAC%" (
+    echo AC Project Folder not found. Please check misspelling.
+	goto retryFolderAC
+)
+
+:retryFolderGC
 set /p buildFolderGC=Choose build folder for GC:
+IF NOT EXIST "%buildFolderGC%" (
+    echo GC Project Folder not found. Please check misspelling.
+	goto retryFolderAC
+)
+
 set "file_AC=%buildFolderAC%\core\pkg\icom\icom_export.sdh" 
 set "file_GC=%buildFolderGC%\adapt\gen\tofac\core\pkg\icom\icom_export.sdh"
 set "search_word=INTERFACEID"
@@ -19,7 +31,6 @@ set "line2="
 REM Find the line containing the search word/phrase in AC
 for /f "tokens=*" %%a in ('type "%file_AC%" ^| findstr "%search_word%"') do (
     set "line1=%%a"
-    set "line1=!line1:*] =!" 
     echo "AC: !line1!"
     goto :found1
 )
@@ -29,7 +40,6 @@ for /f "tokens=*" %%a in ('type "%file_AC%" ^| findstr "%search_word%"') do (
 REM Find the line containing the search word/phrase in GC
 for /f "tokens=*" %%a in ('type "%file_GC%" ^| find "%search_word%"') do (
     set "line2=%%a"
-    set "line2=!line2:*] =!"
     echo "GC: !line2!"
     goto :found2
 )
@@ -49,6 +59,8 @@ if not defined line1 (
     if "!line1!"=="!line2!" (
         echo Your folders are synced. Congratulations
         echo Script finished at %date% %time%
+        echo Press any key to continue...
+        pause > nul
         exit /b
     ) else (
         echo Your folders are not synced. Proceeding with ICOM build syncing...
@@ -59,26 +71,27 @@ if not defined line1 (
     )
 )
 
-
 :proceedWithBuild
 REM can be made more streamlined. For now this works as MVP :)
-:retryA
+:retryBuildAC
 set /p buildAC=Choose build variant for AC:
-IF EXIST "%buildAC%" (
-    echo You have chosen: %buildAC%
+IF EXIST "%buildFolderAC%\%buildAC%.bat" (
+    echo You have chosen: %buildAC%.bat
 ) else (
     echo Batch file not found.
-	goto retryA
+    echo Your chosen directory: "%buildFolderAC%\%buildAC%.bat"
+	goto retryBuildAC
 )
 
 
-:retryB
+:retryBuildGC
 set /p buildGC=Choose build variant for GC:
-IF EXIST "%buildGC%" (
-    echo You have chosen: %buildGC%
+IF EXIST "%buildFolderGC%\%buildGC%.bat" (
+    echo You have chosen: %buildGC%.bat
 ) else (
     echo Batch file not found.
-	goto retryB
+    echo Your chosen directory: "%buildFolderGC%\%buildGC%.bat"
+	goto retryBuildGC
 )
 
 REM ---------- KIV ----------------
@@ -116,8 +129,21 @@ echo running AC batch file %buildAC%
 call "%buildFolderAC%\%buildAC%"
 echo SUCCESS: Successfully run AC project build
 
+
+REM ---AC: GET_PRG, GET_SYM---
+echo AC: Running get_prg and get_sym
+CALL %buildFolderAC%\tool\integration\tool\deliver\core\get_prg.bat
+CALL %buildFolderAC%\tool\integration\tool\deliver\core\get_sym.bat
+
+REM ---GC: GET_PRG, GET_SYM---
+echo GC: Running get_prg and get_sym
+CALL %buildFolderGC%\tool\integration\tool\deliver\core\get_prg.bat
+CALL %buildFolderGC%\tool\integration\tool\deliver\core\get_sym.bat
+
 endlocal
 
 REM end logFile
 echo Script finished at %date% %time% >> %logFile%
+echo Press any key to continue...
+pause > nul
 exit /b
